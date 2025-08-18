b0464ac4dd31aea388b10951c3f32dd0b88f61d4
lucas mateus <lucas.mts1812@gmail.com>
2025-08-18 01:49:48 -0300

    Finished missing methods on PlaceService for findAll, findById, save, findByName and alter

--- src/main/java/br/com/clickbus/challenge/service/PlaceService.java
+++ src/main/java/br/com/clickbus/challenge/service/PlaceService.java
@@ -5,36 +5,37 @@ import br.com.clickbus.challenge.dto.PlaceDTO;
 import br.com.clickbus.challenge.entity.Place;
 import br.com.clickbus.challenge.repository.PlaceRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
-import org.apache.commons.lang3.NotImplementedException;
 
 @Service
 @AllArgsConstructor
 public class PlaceService {
 
+    @Autowired
     private PlaceRepository repository;
 
     public List<Place> findAll() {
-        throw new NotImplementedException("Metodo nao implementado");
+        return repository.findAll();
     }
 
-    public Optional<Place> findById(@NotNull Long id) {
-        throw new NotImplementedException("Metodo nao implementado");
-    }
+    public Optional<Place> findById(@NotNull Long id) { return repository.findById(id);}
 
-    public Place save(@NotNull Place place) {
-        throw new NotImplementedException("Metodo nao implementado");
-    }
+    public Place save(@NotNull Place place) { return repository.save(place); }
 
-    public List<Place> findByName(@NotNull String name) {
-        throw new NotImplementedException("Metodo nao implementado");
-    }
+    public List<Place> findByName(@NotNull String name) { return repository.findByName(name); }
 
     public Place alter(@NotNull Place place,@NotNull PlaceDTO placeDTO) {
-        throw new NotImplementedException("Metodo nao implementado");
+        place.setName(placeDTO.getName());
+        place.setSlug(placeDTO.getSlug());
+        place.setCity(placeDTO.getCity());
+        place.setState(placeDTO.getState());
+        place.setUpdatedAt(LocalDateTime.now());
+        return repository.save(place);
     }
 }
