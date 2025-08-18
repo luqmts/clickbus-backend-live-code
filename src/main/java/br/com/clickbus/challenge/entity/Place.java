5574e9e1ecddcad7c1a82aa81d638eff65314666
lucas mateus <lucas.mts1812@gmail.com>
2025-08-18 01:47:27 -0300

    Implemented Setter for attributes on Place class

--- src/main/java/br/com/clickbus/challenge/entity/Place.java
+++ src/main/java/br/com/clickbus/challenge/entity/Place.java
@@ -5,6 +5,7 @@ import br.com.clickbus.challenge.dto.PlaceDTO;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -15,6 +16,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Place {
